44ae174ebb7a8f372199ee7d4054d5b5725bd67a
Ene Catalin <catalin9100@gmail.com>
2021-06-29 20:37:19 +0300

    Add project files.

---
+++ Project1/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+
+
+using namespace std;
+
+//how many prime numbers are into a vector.
+void main() {
+	int n, i, j, t, k = 0, v[10];
+	cout << "Dimension: ";
+	cin >> n;
+	for (i = 1; i <= n; i++) {
+		cout << "x[" << i << "]=";
+		cin >> v[i];
+	}
+	for (i = 1; i <= n; i++) {
+		t = 1;
+		for (j = 2; j <= v[i] / 2; j++) {
+			if (v[i] % j == 0) {
+				t = 0;
+			}
+		}
+		if (t == 1)
+			k++;
+	}
+	cout << "In the vector are " << k << " prime numbers";
+}
